628138e8780ea125b53512022f677cf1b6dc8d10
Ashutosh Kumar Singh <kumar.ashutosh2006.ak@gmail.com>
2026-06-16 21:33:41 +0530

    Add program to sum two numbers

---
+++ basics/add_two_nos.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+    
+    //taking input 
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    //adding two nos
+    sum =a+b;
+    
+    printf("the sum of two nos is %d",sum);
+
+    return 0;
+}
